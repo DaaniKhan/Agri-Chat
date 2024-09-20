889a7a9ca78f41198382a65fe0cc8ad9452bf8f0
Daanish Khan <70915304+DaaniKhan@users.noreply.github.com>
2024-09-20 14:04:30 +0500

    saving money

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ console.log("First Schedule Call")
 scheduleDailyJob();
 
 // Cron job to update daily_update_time and reschedule the job
-cron.schedule("* * * * *", async () => {
+cron.schedule("00 * * * *", async () => {
     // Update the daily_update_time dynamically
     async function getUpdateTimes(){
         
